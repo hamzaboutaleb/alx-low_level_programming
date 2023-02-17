e2f6478492f29f3188eb2aa99e5e00a4805c0565
boutalebhamza <boutaleb.hm@gmail.com>
2023-02-17 02:28:25 -0800

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+  * main - entry point
+  * Return: 0
+  */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = '0'; ch <= 9; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
