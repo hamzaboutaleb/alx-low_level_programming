4d3a67f4c2310818f9f97a929337b44ba55fc21b
boutalebhamza <boutaleb.hm@gmail.com>
2023-02-21 09:17:33 -0800

    project 0x02

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,8 +8,13 @@
 
 int print_last_digit(int n)
 {
-	int last_digit = n % 10;
+	int last_digit;
+	if (a < 0)
+		n = -n;
 
-	_putchar(last_digit);
+	last_digit = n % 10;
+	
+
+	_putchar(last_digit + '0');
 	return (last_digit);
 }
