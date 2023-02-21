c66be7cfcbed52c369f92dd9108416ba21cadd86
boutalebhamza <boutaleb.hm@gmail.com>
2023-02-21 09:06:23 -0800

    project 0x02

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  * _abs - compute absolute value of number
+  * @n: number
+  *
+  * Return: absolue value of number
+  */
+int _abs(int n)
+{
+	if (n < 0)
+		return (-n);
+	else
+		return (n);
+}
