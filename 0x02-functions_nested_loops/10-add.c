fe4d53aa2e7cb04bfc58c7e18be548861ac9786f
boutalebhamza <boutaleb.hm@gmail.com>
2023-02-21 10:16:25 -0800

    0x02

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * add - add two number
+  * @n: first number
+  * @v: second number
+  * Return: some of two number
+  */
+
+int add(int n, int v)
+{
+	return (n + v);
+}
