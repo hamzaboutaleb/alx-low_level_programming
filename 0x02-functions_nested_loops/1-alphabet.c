f0b67bb363148b1d5b09151fb4e8a800b6e7e9c1
boutalebhamza <boutaleb.hm@gmail.com>
2023-02-21 08:19:25 -0800

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * print_alphabet - print lowercase alphabet to terminal
+  */
+
+void print_alphabet(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+		_putchar(a);
+	_putchar('\n');
+}
