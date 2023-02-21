ea0be6c0608f3461842e2939e2296db9e14df1fe
boutalebhamza <boutaleb.hm@gmail.com>
2023-02-21 09:10:37 -0800

    project 0x02

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h";
+
+/**
+  * print_last_digit - print last digit
+  * @n: number to print its last digit
+  * Return: last digit
+  */
+
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
