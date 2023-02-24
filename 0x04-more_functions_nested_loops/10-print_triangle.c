3bcbda488f645dbba962dc622264442616423a77
boutalebhamza <boutaleb.hm@gmail.com>
2023-02-24 02:03:28 -0800

    task

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,8 +8,9 @@
 void print_triangle(int size)
 {
 	int i, j;
-	
+
 	if (size > 0)
+	{
 		for (i = 1; i <= size; i++)
 		{
 			for (j = 0; j < size - i ; j++)
@@ -17,6 +18,9 @@ void print_triangle(int size)
 			_putchar('#');
 			_putchar('\n');
 		}
+	}
 	else
+	{
 		_putchar('\n');
+	}
 }
