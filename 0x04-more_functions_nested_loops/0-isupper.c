2365a46bbf3db88dfcaf33b2d71508d3a9e0bcb1
boutalebhamza <boutaleb.hm@gmail.com>
2023-02-23 23:59:22 -0800

    0x03

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,12 +2,13 @@
 
 /**
   * _isupper - function check fi number is uppercase
+  * @c: character to be checked
   * Return: 1 if character is upper case otherwise 0
   */
 
 int _isupper(int c)
 {
-	if(c >= 'A' && c <= 'Z')
+	if (c >= 'A' && c <= 'Z')
 		return (1);
 	else
 		return (0);
