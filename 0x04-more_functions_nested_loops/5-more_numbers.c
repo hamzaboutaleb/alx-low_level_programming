d32bd0ab86260d82406bd5e82e26ac99212ae839
boutalebhamza <boutaleb.hm@gmail.com>
2023-02-24 00:28:31 -0800

    0x03

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+  * more_numbers - print numbers from 0 to 14 and 10 times
+  */
+
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i >= 10)
+			{
+				_putchar((i / 10) + '0');
+				_putchar((i % 10) + '0');
+			}
+			else
+			{
+				_putchar(i + '0');
+			}
+		}
+
+		_putchar('\n');
+	}
+}
