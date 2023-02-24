a72a69057acc95d112066c610aed15105283548b
boutalebhamza <boutaleb.hm@gmail.com>
2023-02-24 00:04:54 -0800

    0x03

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+
+
+/**
+  * mul - multiple two number
+  * @a : first number
+  * @b : second number
+  * Return: Multiplication of two numbers
+  */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,3 +2,4 @@
 
 int _isupper(int c);
 int _isdigit(int);
+int mul(int, int);
