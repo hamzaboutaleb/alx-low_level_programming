9f44036124f6b1a62e4d38051c0a6c6c82db5ed6
boutalebhamza <boutaleb.hm@gmail.com>
2023-02-27 02:04:57 -0800

    0x05

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#incldue "main.h"
+/**
+  * swap_int - swap value of two inetegers
+  * @a: first int
+  * @b: second int
+  */
+void swap_int(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp
+}
