5d74d9f63741c50f0b31cdfc6afd8c6918cfb90f
boutalebhamza <boutaleb.hm@gmail.com>
2023-02-27 02:49:17 -0800

    0x05

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+  * puts2 -- prints every pair char
+  * @str: string
+  */
+
+void puts2(char *str)
+{
+	int i;
+	int len;
+
+	for (len = 0; str[len] != '\0'; len++)
+	{
+	}
+
+	for (i = 0; i < len; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
