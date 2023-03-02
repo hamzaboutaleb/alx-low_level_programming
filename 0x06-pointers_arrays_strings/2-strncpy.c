e4229eb6f4685328c031c8d75ad4b4beb22f0912
boutalebhamza <boutaleb.hm@gmail.com>
2023-03-02 09:28:02 -0800

    0x06

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+  * _strncpy - copy n character of string from src to dest
+  * @dest: pointer to dest memory
+  * @src: pointer to string
+  * @n: number of byte
+  * Return: pointer to dest
+  */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	if (dest[i] != '\0')
+		dest[i] = '\0';
+
+	return (dest);
+}
