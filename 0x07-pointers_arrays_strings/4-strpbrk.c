96fd25b9772ce535cdc737e2651e2058b5657485
boutalebhamza <boutaleb.hm@gmail.com>
2023-03-06 15:26:30 -0800

    0x07 tasks

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -11,7 +11,7 @@ char *_strpbrk(char *s, char *accept)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; accept[j] != '\0'; k++)
+		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (accept[j] == s[i])
 				return (s + i);
