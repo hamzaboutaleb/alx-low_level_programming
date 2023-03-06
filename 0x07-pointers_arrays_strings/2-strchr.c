a7332a1e608e2d36994c3d948e3c50ca00c096b2
boutalebhamza <boutaleb.hm@gmail.com>
2023-03-06 13:54:33 -0800

    0x07 tasks

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -3,10 +3,11 @@
   * _strchr -  locates a character in a string.
   * @s: pointer to string
   * @c: character
+  * Return: pointer to char
   */
 char *_strchr(char *s, char c)
 {
-	int i;
+	unsigned int i;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
@@ -16,5 +17,5 @@ char *_strchr(char *s, char c)
 		}
 	}
 
-	return NULL;
+	return (NULL);
 }
