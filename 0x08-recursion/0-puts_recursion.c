c5eaefd3e855e4af21ccb0c75818e62d4e7a75d0
boutalebhamza <boutaleb.hm@gmail.com>
2023-03-08 04:22:45 -0800

    0x08 tasks

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+  * _puts_recursion - print string recursive way
+  * @s: strng to print
+  */
+void _puts_recursion(char *s)
+{
+	if (s[0] == '\0')
+		return ();
+	_putchar(s[0]);
+	s++;
+	_puts_recursion(s);
+}
