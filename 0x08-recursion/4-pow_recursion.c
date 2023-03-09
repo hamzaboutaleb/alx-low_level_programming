f8316bfa95bc41732be0ca5b81fc2a490cd59155
boutalebhamza <boutaleb.hm@gmail.com>
2023-03-09 06:12:27 -0800

    0x08 tasks

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,14 @@
+/**
+  * _pow_recusrion - returns the value of x raised to the power of y
+  * @x: base
+  * @y: power
+  * Return: power
+  */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 1)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+}
