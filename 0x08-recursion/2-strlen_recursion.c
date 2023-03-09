b41e289370bf9e376711cf64191624b74860f36d
boutalebhamza <boutaleb.hm@gmail.com>
2023-03-09 06:04:24 -0800

    0x08 tasks

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,13 +1,14 @@
 
 /**
-  * _strlen_recursion -  length of a string
+  * _strlen_recursion - length of a string
   * @s: string
+  * Return: lenght
   */
 
 int _strlen_recursion(char *s)
 {
 	if (s[0] == '\0')
-		return 0;
+		return (0);
 
-	return 1 + _strlen_recursion(s + 1);
+	return (1 + _strlen_recursion(s + 1));
 }
