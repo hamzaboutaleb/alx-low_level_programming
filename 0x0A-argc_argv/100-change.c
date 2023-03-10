7546c546832149b02f34b9f600171fe7c5ae3031
boutalebhamza <boutaleb.hm@gmail.com>
2023-03-10 02:43:22 -0800

    0x0A tasks

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,28 +2,15 @@
 #include <stdlib.h>
 
 /**
-  * main - prints the minimum number of coins to make change for an amount of money
-  * @argc: numbero of args
-  * @argv: array of args
-  * Return: 0 || 1
+  * to_coins - minimum number of coins to make change for an amount of money.
+  * @n: cents
+  * Return: number of coins
   */
-
-int main(int argc, char **argv)
+int to_coins(int n)
 {
-	int cents, i, sum;
+	int cents;
 
-	if (argc != 2)
-	{
-		puts("Error");
-		return (1);
-	}
-	cents = atoi(argv[1]);
-	if (cents < 0)
-	{
-		puts("0");
-		return (0);
-	}
-	
+	cents = n;
 	while (cents != 0)
 	{
 		if (cents % 25 == 0)
@@ -52,6 +39,32 @@ int main(int argc, char **argv)
 			cents--;
 		}
 	}
+	return (cents);
+}
+
+/**
+  * main - prints the minimum number of coins
+  * @argc: numbero of args
+  * @argv: array of args
+  * Return: 0 || 1
+  */
+
+int main(int argc, char **argv)
+{
+	int cents, i, sum;
+
+	if (argc != 2)
+	{
+		puts("Error");
+		return (1);
+	}
+	cents = atoi(argv[1]);
+	if (cents < 0)
+	{
+		puts("0");
+		return (0);
+	}
+	sum = to_coins(cents);
 	printf("%d\n", sum);
 	return (0);
 }
