a171fe82f08e04dd5b66ec8bc532b8f347d3aea6
boutalebhamza <boutaleb.hm@gmail.com>
2023-03-10 01:42:36 -0800

    0x0A tasks

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-
+/**
+  * main - print file name
+  * @argc: length of arg
+  * @argv: pointer to arg
+  * Return: 0
+  */
 int main(int argc, char **argv)
 {
 	printf("%s\n", argv[0]);
