3d059a796468ba80869ef0a9f36fbd963cd54d69
boutalebhamza <boutaleb.hm@gmail.com>
2023-03-10 01:50:18 -0800

    0x0A tasks

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-
-int main(int argc, char **argv)
+/**
+  * main - print number of args
+  * @argc: number of args
+  * @argv: array f args
+  * Return: 0
+  */
+int main(int argc, char **argv __attribute__((unused)))
 {
 	printf("%d\n" argc - 1);
 	return (0);
