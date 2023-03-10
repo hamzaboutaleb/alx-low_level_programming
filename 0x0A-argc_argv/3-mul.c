4e44a2bfadc8bf8a3ab689f277dd864588100cfe
boutalebhamza <boutaleb.hm@gmail.com>
2023-03-10 01:57:25 -0800

    0x0A tasks

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+  * main - print multiplcation of 2 number
+  * @argc: number of args
+  * @argv: arrya of args
+  * Return: 0 || 1
+  */
+int main(int argc, char **argv)
+{
+	if (aggc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", argv[1] + argv[2]);
+	return (0);
+}
