6b117c6a8b2db33ee7e6636cc6299847c8f078e7
boutalebhamza <boutaleb.hm@gmail.com>
2023-03-10 01:47:06 -0800

    0x0A tasks

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,7 +5,7 @@
   * @argv: array of args
   * Return: 0
   */
-int main(int argc __attribute((unused)), char **argv)
+int main(int argc __attribute__((unused)), char **argv)
 {
 	printf("%s\n", *argv);
 	return (0);
