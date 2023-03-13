5c27eef0821bd9011a312b2dfd5b2cc5c0508cfe
boutalebhamza <boutaleb.hm@gmail.com>
2023-03-13 02:47:51 -0700

    0x0B tasks

---
+++ 0x0B-malloc_free/100-argtostr.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+/**
+  * argtostr - concatenate all arg
+  * @ac: number of args
+  * @av: pointer to args
+  * Return: pointer to string
+  */
+char *argtostr(int ac, char **av)
+{
+	char *p;
+	int size, i, j, k = 0;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+			size++;
+		size++;
+	}
+	p = (char *) malloc(sizeof(char) * size);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; j < av[i][j]; j++)
+			p[k++] = av[i][j];
+		p[k++] = '\n';
+	}
+	return (p);
+}
