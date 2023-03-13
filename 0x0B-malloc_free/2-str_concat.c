d144a63a64e0f2740caf616653683647f2bf224f
boutalebhamza <boutaleb.hm@gmail.com>
2023-03-13 02:08:46 -0700

    0x0B tasks

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,7 +9,10 @@ char *str_concat(char *s1, char *s2)
 {
 	char *p;
 	int i, size_s1 = 0, size_s2 = 0;
-	
+
+	if (s1 == NULL && s2 == NULL)
+		return (NULL);
+
 	if (s1 != NULL)
 		for (i = 0; s1[i] != '\0'; i++)
 			size_s1++;
