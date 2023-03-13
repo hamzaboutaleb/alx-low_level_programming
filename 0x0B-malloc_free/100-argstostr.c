900ea4c862cdfe2a29d298689b85b94039f9266d
boutalebhamza <boutaleb.hm@gmail.com>
2023-03-13 02:50:27 -0700

    0x0B tasks

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -23,7 +23,7 @@ char *argstostr(int ac, char **av)
 
 	for (i = 0; i < ac; i++)
 	{
-		for (j = 0; j < av[i][j]; j++)
+		for (j = 0; av[i][j] != '\0'; j++)
 			p[k++] = av[i][j];
 		p[k++] = '\n';
 	}
