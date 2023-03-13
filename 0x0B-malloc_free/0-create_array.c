e84ea8cfa455f72389811bb24d36c5f2199fbbad
boutalebhamza <boutaleb.hm@gmail.com>
2023-03-13 01:23:40 -0700

    0x0B tasks

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+
+char *create_array(unsigned int size, char c)
+{
+	int *p, i;
+	p = (char *) malloc(sizeof(char) * size + 1);
+
+	if (p == NULL)
+		return (NULL);
+	
+	for (i = 0; i < size; i++)
+	{
+		p[i] = 'c';
+	}
+	p[i] = '\0';
+
+	return (p);
+}
