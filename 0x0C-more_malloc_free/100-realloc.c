0b65738e9193854a87df169e13f6532aa0f1ed06
boutalebhamza <boutaleb.hm@gmail.com>
2023-03-16 15:46:27 -0700

    0x0C tasks

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{	
+	void *p;
+	char *p1, *p2;
+	int min, i;
+
+	if (new_size == old_size)
+		return (ptr);
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (ptr == NULL)
+	{
+		ptr = malloc(new_size);
+		return (ptr);
+	}
+
+	if (old_size > new_size)
+		min = new_size;
+	else
+		min = old_size;
+
+	p = malloc(new_size);
+	p1 = (char *) p;
+	p2 = (char *) ptr;
+
+	for (i = 0; i < min; i++)
+		p1[i] = p2[i];
+
+	free(ptr);
+
+	return (p);
+}
