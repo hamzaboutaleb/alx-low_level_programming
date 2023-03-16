d5c96b1c7b554970a78ff8ce5af7a9cac4908bb0
boutalebhamza <boutaleb.hm@gmail.com>
2023-03-16 11:28:45 -0700

    0x0C tasks

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,7 +13,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	void *p;
 	unsigned int i, j;
 
-	if (nmemb <= 0 || size <= 0)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
 
 	p = malloc(size * nmemb);
