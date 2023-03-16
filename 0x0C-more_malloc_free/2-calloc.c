4d818eaf8a04bc840fa369f51e0d70ce3e5094af
boutalebhamza <boutaleb.hm@gmail.com>
2023-03-16 11:27:35 -0700

    0x0C tasks

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,22 +9,21 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *p;
-	void *p1;
+	char *p1;
+	void *p;
 	unsigned int i, j;
 
 	if (nmemb <= 0 || size <= 0)
 		return (NULL);
 
-	p = (char *) malloc(size * nmemb);
-
+	p = malloc(size * nmemb);
+	p1 = (char *) p;
 	if (p == NULL)
 		return (NULL);
 
 	for (i = 0; i < nmemb; i++)
 		for (j = 0; j < size; j++)
-			*(p + i + j) = 0;
-	p1 = (void *) p;
+			*(p1 + i + j) = 0;
 
-	return (p1);
+	return (p);
 }
