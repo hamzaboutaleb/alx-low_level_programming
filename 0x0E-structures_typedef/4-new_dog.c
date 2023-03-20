20c104d1f75d3dd04c32a9459f974e837454a9d8
boutalebhamza <boutaleb.hm@gmail.com>
2023-03-20 05:04:13 -0700

    0x0E tasks

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+  * new_dog - alloavte memory for dog struct
+  * @name: name
+  * @age: age
+  * @owner: owner of dog
+  * Return: pointer to memory or NULL if its fails
+  */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *d;
+
+	d = (dog_t *) malloc(sizeof(dog_t));
+	if (d == NULL)
+		return (NULL);
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+
+	return (d);
+}
