fa63100aaaf18ae5ee11b524337fc31da12c693f
boutalebhamza <boutaleb.hm@gmail.com>
2023-03-20 04:42:00 -0700

    0x0E tasks

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,14 @@
+
+/**
+  * init_dog - initiate dog struct
+  * @d: pointer to dog struct
+  * @name: name
+  * @age: age
+  * @owner: owner of dog
+  */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner
+}
