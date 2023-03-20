21372339406676d23ac5d98f0747c1f7c2ca9604
boutalebhamza <boutaleb.hm@gmail.com>
2023-03-20 05:18:05 -0700

    0x0E tasks

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -17,9 +17,20 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 
 	d->name = (char *) malloc(strlen(name) * sizeof(char) + 1);
+	if (d->name == NULL)
+	{
+		free(d);
+		return (NULL);
+	}
 	strcpy(d->name, name);
 	d->age = age;
 	d->owner = (char *) malloc(strlen(owner) + sizeof(char) + 1);
+	if (d->owner == NULL)
+	{
+		free(d->name);
+		free(d);
+		return (NULL);
+	}
 	strcpy(d->owner, owner);
 	return (d);
 }
