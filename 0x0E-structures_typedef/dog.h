c5905b161a97febc3af205aa95ade72cca12856c
boutalebhamza <boutaleb.hm@gmail.com>
2023-03-20 04:37:02 -0700

    0x0E tasks

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
-
-struct dog {
+#ifndef DOG_H
+#define DOG_H
+/**
+  * struct dog - structure to define animal dog
+  * @name: name of dog
+  * @age: age of dog
+  * @owner: dog owner
+  */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
 };
+#endif
