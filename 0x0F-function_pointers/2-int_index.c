ea056a38d71f5331d46af242fa3a8a95fde7a79e
boutalebhamza <boutaleb.hm@gmail.com>
2023-03-22 15:33:09 -0700

    0x0F tasks

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,21 @@
+/**
+  * int_index - search for integer
+  * @array: array to search in
+  * @size: size of arrau
+  * @cmp: function use to verfie
+  */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+	
+	if (size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+	return (-1);
+
+}
