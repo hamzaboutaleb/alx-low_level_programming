f7bd2d1e627d917451da64773a0d92f9e0b09fab
boutalebhamza <boutaleb.hm@gmail.com>
2023-03-22 15:06:14 -0700

    0x0F tasks

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+/**
+  * array_iterator - loop over an array and perfom action
+  * @size: size of array
+  * @action: action to perform
+  */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	if (action == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
