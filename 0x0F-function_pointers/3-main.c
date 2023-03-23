59e37eadf6304fbb15447ec5e9a5603da47c6731
boutalebhamza <boutaleb.hm@gmail.com>
2023-03-22 17:00:14 -0700

    0x0F tasks

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,6 +1,23 @@
 #include "3-calc.h"
-
-int main(void)
+#include <stdio.h>
+int main(int argc, char *argv[])
 {
+	int (*op)(int ,int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	op = get_op_func(argv[2]);
+
+	if (!op)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	printf("%d\n", op(atoi(argv[1]), atoi(argv[3])));
 	return (0)
 }
