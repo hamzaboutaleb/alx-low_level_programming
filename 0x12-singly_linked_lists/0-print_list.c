9e8be9cabaf2d3d4a321950ddfc3742b00b6f510
hamzaboutaleb <hamza-boutaleb@live.fr>
2023-03-30 15:37:49 +0000

    project 0x12

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+size_t print_list(const list_t *h)
+{
+    size_t n = 0;
+
+
+    while (h != NULL)
+    {
+        if (h->str == NULL)
+            printf("[0] (nill)\n");
+        else
+            printf("[%d] %s\n", h->len, h->str);
+        n++;
+        h = h->next;
+    }
+    return n;
+}
