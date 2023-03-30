4830f0d4f87a10684b103b4cf509e8802b504fc7
hamzaboutaleb <hamza-boutaleb@live.fr>
2023-03-30 15:46:49 +0000

    project 0x12

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,21 +1,26 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "lists.h"
-
+/**
+ * print_list - print list
+ * @h: pointer to list
+ * Return: length of list
+ */
 size_t print_list(const list_t *h)
 {
-    size_t n = 0;
-    if (h == NULL)
-        return (0);
+	size_t n = 0;
 
-    while (h != NULL)
-    {
-        if (h->str == NULL)
-            printf("[0] (nill)\n");
-        else
-            printf("[%d] %s\n", h->len, h->str);
-        n++;
-        h = h->next;
-    }
-    return n;
+	if (h == NULL)
+		return (0);
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nill)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		n++;
+		h = h->next;
+	}
+	return (n);
 }
