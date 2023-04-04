646d55d1a09e6a90e624ed09c4bb52069fe41649
hamzaboutaleb <hamza-boutaleb@live.fr>
2023-04-04 22:38:20 +0000

    project 0x13

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -7,32 +7,32 @@
 */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-    listint_t *temp;
-    listint_t *current;
+	listint_t *temp;
+	listint_t *current;
 
-    if (*head == NULL)
-        return (-1);
+	if (*head == NULL)
+		return (-1);
 
-    if (index == 0)
-    {
-        temp = (*head)->next;
-        free(*head);
-        *head = temp;
-        return (1);
-    }
+	if (index == 0)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+		return (1);
+	}
 
-    current = *head;
-    while ((index - 1) && current)
-    {
-        current = current->next;
-        index--;
-    }
-    if(current == NULL)
-        return (-1);
+	current = *head;
+	while ((index - 1) && current)
+	{
+		current = current->next;
+		index--;
+	}
+	if (current == NULL)
+		return (-1);
 
-    temp = current->next;
-    current->next = temp->next;
-    free(temp);
-    return (1);
+	temp = current->next;
+	current->next = temp->next;
+	free(temp);
+	return (1);
 
 }
