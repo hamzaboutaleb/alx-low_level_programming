a1d0a8df19124a83a72c72782899c6ecccd4de87
hamzaboutaleb <hamza-boutaleb@live.fr>
2023-04-04 21:26:19 +0000

    project 0x13

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -9,8 +9,9 @@ int pop_listint(listint_t **head)
 	listint_t *temp;
 	int n;
 
-	if (head == NULL)
+	if (head == NULL || *head == NULL)
 		return (0);
+
 	temp = (*head)->next;
 	n = (*head)->n;
 	free(*head);
