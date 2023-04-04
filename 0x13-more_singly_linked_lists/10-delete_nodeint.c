1fc04770a6eb7812133293d8f352ba4972e02ac7
hamzaboutaleb <hamza-boutaleb@live.fr>
2023-04-04 22:14:32 +0000

    project 0x13

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -16,6 +16,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
     if (index == 0)
     {
         temp = (*head)->next;
+        free(*head);
         *head = temp;
         return (1);
     }
