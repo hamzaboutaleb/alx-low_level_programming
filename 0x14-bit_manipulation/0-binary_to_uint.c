ee3626e26b602c961f1b7d3ae43e46fdebd20826
hamzaboutaleb <hamza-boutaleb@live.fr>
2023-04-10 10:28:24 +0000

    project 0x14

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <string.h>
 #include "main.h"
 /**
  * binary_to_uint - convert binary number to decimal
@@ -9,22 +10,25 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int result;
 	unsigned int base;
+	int len, i = 0;
 
 	if (b == NULL)
 		return (0);
 
+	len = strlen(b);
 	result = 0;
 	base = 1;
-	while (*b)
+	while (b[len - i - 1])
 	{
-		if (*b != '0' || *b != '1')
+		if (b[len - i - 1] != '0' && b[len - i - 1] != '1')
 			return (0);
 
-		if (*b == '1')
+		if (b[len - i - 1] == '1')
 		{
 			result += 1 * base;
 		}
 
+		i++;
 		base *= 2;
 	}
 
