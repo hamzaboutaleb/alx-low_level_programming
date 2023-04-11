872e99a4a0692f1c6fdf211403b8a6b1fcddcee3
hamzaboutaleb <hamza-boutaleb@live.fr>
2023-04-11 16:09:56 +0000

    project 0x15

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,27 @@
+#include <string.h>
+#include "main.h"
+/**
+ * create_file - create file
+ * @filename: name of file
+ * @text_content: text to write in file
+ * Return: 0 || -1
+*/
+int create_file(const char *filename, char *text_content)
+{
+	int fo, fw;
+	size_t len;
+
+	if (filename == NULL)
+		return (0);
+
+	fo = open(filename, O_RDWR | O_CREAT | O_TRUNC, 600);
+	if (fo == -1)
+		return (0);
+	len = strlen(text_content);
+	fw = write(fo, text_content, len);
+
+	if (fw == -1)
+		return (0);
+
+	return (1);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,5 +5,6 @@
 #include <fcntl.h>
 #include <unistd.h>
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 int _putchar(char c);
 #endif
