17425163845a8efc814023653c5411084abd1a16
hamzaboutaleb <hamza-boutaleb@live.fr>
2023-04-11 16:12:29 +0000

    project 0x15

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,19 +9,19 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fo, fw;
-	int len;
+	int len = 0;
 
 	if (filename == NULL)
-		return (0);
+		return (-1);
 
 	fo = open(filename, O_RDWR | O_CREAT | O_TRUNC, 600);
 	if (fo == -1)
-		return (0);
+		return (-1);
 	len = strlen(text_content);
 	fw = write(fo, text_content, len);
 
 	if (fw == -1)
-		return (0);
+		return (-1);
 
 	close(fo);
 	return (1);
