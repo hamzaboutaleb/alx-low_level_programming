20a7b19256680477b759b5f609d809b7614f4446
hamzaboutaleb <hamza-boutaleb@live.fr>
2023-04-11 16:58:50 +0000

    project 0x15

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -17,7 +17,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 		return (1);
 
-	fo = open(filename, O_APPEND);
+	fo = open(filename, O_APPEND | O_WRONLY);
 
 	if (fo == -1)
 		return (-1);
