c417c45643a7bd684abd7c4fa9d31b7135650ee2
boutalebhamza <boutaleb.hm@gmail.com>
2023-05-18 05:03:36 -0700

    dlist tasks

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -15,6 +15,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	if (new_node == NULL)
 		return (NULL);
+	if (!(head))
+	{
+		(*head) = new_node;
+		return (new_node);
+	}
 	new_node->n = n;
 	new_node->next = (*head);
 	new_node->prev = NULL;
