bd924724c39a401e8d0bd86dc34f353996b1262b
boutalebhamza <boutaleb.hm@gmail.com>
2023-05-18 06:17:23 -0700

    dlist tasks

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -15,7 +15,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *curr;
 
 	node = (dlistint_t *) malloc(sizeof(dlistint_t));
-	if (!(*head))
+	if (!(*h))
 		return (NULL);
 	if (!node)
 		return (NULL);
