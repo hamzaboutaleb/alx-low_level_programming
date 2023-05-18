98bba32e85d383b38e6746a190ca69c6552f4755
boutalebhamza <boutaleb.hm@gmail.com>
2023-05-18 05:38:02 -0700

    dlist tasks

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -25,7 +25,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		(*h) = node;
 		return (node);
 	}
-	
 	if (idx == 0)
 	{
 		node->next = (*h);
@@ -41,7 +40,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (!curr && idx > 0)
 		return (NULL);
-	
+
 	node->next = curr;
 	node->prev = curr->prev;
 	(curr->prev)->next = node;
