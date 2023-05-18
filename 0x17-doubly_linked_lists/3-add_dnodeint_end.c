0cc71963a3a22c82f98eb4dcf88ffeeb99e26740
boutalebhamza <boutaleb.hm@gmail.com>
2023-05-18 05:11:27 -0700

    dlist tasks

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+  * add_dnodeint_end - add node at the end of dbly linked list
+  * @head: head
+  * @n: value
+  * Return: node
+  */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *node;
+	dlistint_t *curr;
+
+	node = (dlistint_t *) malloc(sizeof(dlistint_t));
+	if (!node)
+		return (NULL);
+	node->n = n;
+	curr = *head;
+
+	while(curr->next)
+		curr = curr->next;
+	node->prev = curr;
+	curr->next = node;
+
+	return (node);
+}
