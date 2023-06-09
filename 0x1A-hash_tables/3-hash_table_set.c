e7e723d7e07500c9706d4f6f18aa3fac2623dbb9
boutalebhamza <boutaleb.hm@gmail.com>
2023-06-09 10:52:57 -0700

    task 3

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,52 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+  * hash_table_set - add node to hash table
+  * @ht: pointer to hash table
+  * @key: key
+  * @value: balue
+  * Return: 1 if successed 0 othrwise
+  */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *curr_node, *new_node;
+	unsigned long int index;
+
+	if (ht == NULL || !key || !value)
+		return (0);
+
+	index = key_index(key, ht->size);
+	curr_node = ht->array[index];
+
+	if (!curr_node)
+	{
+		new_node = (hash_node_t *)malloc(sizeof(hash_node_t));
+		if (!new_node)
+			return (0);
+		new_node->value = strdup(value);
+		new_node->key = strdup(key);
+		curr_node = new_node;
+	}
+
+	while (!curr_node)
+	{
+		if (strcmp(key, curr_node->key) == 0)
+		{
+			free(curr_node->value);
+			curr_node->value = strdup(value);
+			return (1);
+		}
+		curr_node = curr_node->next;
+	}
+
+	new_node = (hash_node_t *)malloc(sizeof(hash_node_t));
+	if (!new_node)
+		return (0);
+	new_node->value = strdup(value);
+	new_node->key = strdup(key);
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+	return (1);
+}
