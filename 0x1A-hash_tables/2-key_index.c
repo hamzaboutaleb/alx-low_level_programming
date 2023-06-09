0af7c0dce4218429e33e50eadc455fb65903c0bd
boutalebhamza <boutaleb.hm@gmail.com>
2023-06-09 10:09:49 -0700

    task 2

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,15 @@
+#include "hash_table.h"
+#include <stdlib.h>
+
+/**
+  * key_index - return index for key
+  * @key: key to hash it
+  * @size: size of hash table
+  * Return: index
+  */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+    unsigned long int index = hash_djb2(key) % size;
+
+    return index;
+}
