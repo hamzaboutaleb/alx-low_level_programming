250dca865fb7bca3a41523819bbf7f353c848eae
boutalebhamza <boutaleb.hm@gmail.com>
2023-06-09 14:42:14 -0700

    print table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,38 @@
+#include "hash_tables.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * hash_table_print - print hash tavle values
+  * @ht: pointer to hash table
+  */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *node;
+	int i, comma = 0;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] == NULL)
+			continue;
+
+		node = ht->array[i];
+
+		while (node != NULL)
+		{
+			if (comma == 1)
+				printf(", ");
+
+			printf("'%s': '%s'", node->key, node->value);
+			node = node->next;
+
+			if (comma == 0)
+				comma = 1;
+		}
+	}
+	printf("}\n");
+}
