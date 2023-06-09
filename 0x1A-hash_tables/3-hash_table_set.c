900a93aaf86f9c6a4f4bce48eb4473ed130cd16f
boutalebhamza <boutaleb.hm@gmail.com>
2023-06-09 15:17:20 -0700

    set

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -27,7 +27,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			return (0);
 		new_node->value = strdup(value);
 		new_node->key = strdup(key);
-        return (1);
+		ht->array[index] = new_node;
+		return (1);
 	}
 
 	while (!curr_node)
